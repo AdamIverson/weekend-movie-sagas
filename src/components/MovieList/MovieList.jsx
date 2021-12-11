aefb4cafc2bb162c6b61579fc9e0e4241d5ffe33
Adam Iverson <aciverso@gmail.com>
2021-12-11 09:59:58 -0600

    good lord clicking on a movie poster navigates to the details page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { useHistory } from 'react-router-dom'
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const history = useHistory();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    // const handleSelectPet = (movie) => {
+    //     history.pushState('/details');
+    // }
 
     return (
         <main>
@@ -17,9 +23,11 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
+                        <div key={movie.id} onClick={e => history.push('/details')} >
                             <h3>{movie.title}</h3>
                             <img src={movie.poster} alt={movie.title}/>
+                            <p>{movie.description}</p>
+                            
                         </div>
                     );
                 })}
