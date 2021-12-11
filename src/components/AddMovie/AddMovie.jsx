8233d19c8dda78cabb53322097714ca2a3a7483b
Adam Iverson <aciverso@gmail.com>
2021-12-11 08:42:20 -0600

    form wireframed with labels

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,6 +1,26 @@
 function AddMovie() {
   return (
-    <p>Add Movie Page</p>
+    <div>
+      <p>Add Movie Page</p>
+
+        <form>
+          <input placeholder="Movie Title" />
+          <label>Add Movie Title</label>
+          <br></br>
+          <input placeholder="Movie Poster URL" />
+          <label>Add Movie Poster URL</label>
+          <br></br>
+          <textarea placeholder="Add Movie Description"></textarea>
+          <label>Add Movie Description</label>
+          <br></br>
+          <select>
+            <option disabled value='0'>
+              Select Genre
+            </option>
+          </select>
+          <label>Select Movie Genre</label>
+        </form>
+    </div>
   )
 };
 
