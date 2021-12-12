fe209fd2f42af8b1013afdb70a2353b34f570e38
Adam Iverson <aciverso@gmail.com>
2021-12-12 09:11:42 -0600

    brick wall on the detailGenre

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,56 +1,51 @@
-import { useHistory } from 'react-router-dom'
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import MovieList from '../MovieList/MovieList';
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import MovieList from "../MovieList/MovieList";
 
 function Details() {
-  const selectedGenre = useSelector((store) => store.genres)
+  const selectedGenre = useSelector((store) => store.genres);
   const selectedMovie = useSelector((store) => store.selectedMovie);
   const history = useHistory();
   const dispatch = useDispatch();
 
-  console.log('in DETAILS Movie', selectedMovie);
-  console.log('in DETAILS GENRE SELECTED GENRE', selectedGenre);
-  useEffect(() => {
-
-    dispatch({ type: 'FETCH_GENRES'})
-  }, []);
+  console.log("in DETAILS Movie", selectedMovie);
+  console.log("in DETAILS GENRE SELECTED GENRE", selectedGenre);
+  // useEffect(() => {
+  //   dispatch({ type: "FETCH_GENRES" });
+  // }, []);
 
   const navigateHome = () => {
-    history.push("/")
-  }
+    history.push("/");
+  };
 
-  function isolateIdGenre () {
-    if(selectedGenre.id === selectedMovie.id)
-    return (
-      <section>
-        {selectedGenre.map((genre) => {
+  function isolateIdGenre() {
+    console.log("in ISOLATED ID GENRE");
 
-        })
-      </section>
-    )
-  }
+    for (let genre of selectedGenre) {
+      if (genre.id === selectedMovie.genre_id) return (<p>{genre.name}</p>);
+    }
+    return;
+  };
+
   return (
     <section>
       <p>Details Page</p>
 
       {/* <p>{selectedMovie.title}</p> */}
-      {
-        selectedMovie.title ? (
-          <>
-            <h2>{selectedMovie.title}</h2>
-            <img src={selectedMovie.poster} />
-            <p>{selectedMovie.description}</p>
-            {/* <p>{isolateIdGenre()}</p> */}
-            <p>{selectedMovie.name}</p>
-          </>
-        ) : (
-          <p>No Movie Selected</p>
-        )
-      }
+      {selectedMovie.title ? (
+        <>
+          <h2>{selectedMovie.title}</h2>
+          <img src={selectedMovie.poster} />
+          <p>{selectedMovie.description}</p>
+          <p>{selectedGenre.name}</p>
+        </>
+      ) : (
+        <p>No Movie Selected</p>
+      )}
       <button onClick={navigateHome}>Back To List</button>
     </section>
-  ) 
-};
+  );
+}
 
 export default Details;
